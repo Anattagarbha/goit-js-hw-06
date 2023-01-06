ba27b2748bd7aaa31ac0cb8d84c9844b8dd59ab7
Anattagarbha <ruslanvic@gmail.com>
2023-01-06 14:07:57 +0200

    creat foreach cycle, print data

--- js/task-01.js
+++ js/task-01.js
@@ -2,3 +2,8 @@
 
 const totalCategories = document.querySelectorAll("li.item");
 console.log("Number of categories: ", totalCategories.length);
+
+totalCategories.forEach((element) => {
+  console.log(`Категория: ${element.firstElementChild.textContent}, 
+    Количество элементов: ${element.lastElementChild.children.length}`);
+});
