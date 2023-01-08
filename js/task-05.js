e08958d3cfc1d5d0f0b0775c60d4c8dd0d1b21e6
Anattagarbha <ruslanvic@gmail.com>
2023-01-08 19:45:16 +0200

    add event listener

--- js/task-05.js
+++ js/task-05.js
@@ -1,2 +1,10 @@
 let textInput = document.querySelector("#name-input");
 let textOutput = document.querySelector("#name-output");
+
+textInput.addEventListener("input", (event) => {
+  if (event.target.value === "") {
+    textOutput.textContent = "Anonymous";
+  } else {
+    textOutput.textContent = event.target.value;
+  }
+});
