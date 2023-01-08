5bb46a254defe187e4b36aa674f3c1d16473fe61
Anattagarbha <ruslanvic@gmail.com>
2023-01-08 18:43:07 +0200

    add plus listener

--- js/task-04.js
+++ js/task-04.js
@@ -2,3 +2,8 @@ let counterValue = 0;
 let displayCounter = document.querySelector("#value");
 let plusBtn = document.querySelector('button[data-action="increment"]');
 let minusBtn = document.querySelector('button[data-action="decrement"]');
+
+plusBtn.addEventListener("click", () => {
+  counterValue++;
+  displayCounter.textContent = counterValue;
+});
