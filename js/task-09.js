74d1f3c7ead7f79092bcbed02a7163aa66eb15b0
Anattagarbha <ruslanvic@gmail.com>
2023-01-10 19:35:56 +0200

    change bgn color

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+"use strict";
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+let inputBtn = document.querySelector("button.change-color");
+let body = document.querySelector("body");
+let colorValue = document.querySelector("span.color");
+
+inputBtn.addEventListener("click", (event) => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorValue.textContent = color;
+});
