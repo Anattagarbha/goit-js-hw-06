afe8927048a90bc8011c01ece0630c814cf61361
Anattagarbha <ruslanvic@gmail.com>
2023-01-10 18:56:35 +0200

    create form handler

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+let formInput = document.querySelector(".login-form");
+formInput.addEventListener("submit", submitHandler);
 
+function submitHandler(event) {
+  event.preventDefault();
+  let {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Please enter a valid email and password");
+  }
+  let userData = { email: email.value, password: password.value };
+  console.log(userData);
+  event.currentTarget.reset();
+}
