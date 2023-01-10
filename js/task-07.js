517cac6e0884502ca4c1b1b183a75fac021dca73
Anattagarbha <ruslanvic@gmail.com>
2023-01-10 13:09:17 +0200

    change text look

--- js/task-07.js
+++ js/task-07.js
@@ -1,4 +1,8 @@
 "use strict";
 
-let inputValue = document.getElementById("input#font-size-control");
-let textLook = document.getElementById("span#text");
+let inputValue = document.querySelector("input#font-size-control");
+let textLook = document.querySelector("span#text");
+
+inputValue.addEventListener("change", (event) => {
+  textLook.style.fontSize = `${event.target.value}px`;
+});
