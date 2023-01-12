c31f183861d9a1e19fc25d2cb8ade983cefebc90
Anattagarbha <ruslanvic@gmail.com>
2023-01-12 17:28:37 +0200

    pseudocode boxes

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,40 @@
+// 1 - select btn nodes
+// 2 - init input value
+// 3 - select elements node
+// 4 - create boxes...
+// 5 - add create listener function
+// 6 - add destroy listener function
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const inputValue = document.querySelector("#controls>input");
+const boxes = document.querySelector("#boxes");
+
+const createBoxes = (amount) => {
+  let totalElements = [];
+  for (let i = 0; i < amount; i++) {
+    const element = document.createElement("div");
+    element.style.width = `${30 + 10 * i}px`;
+    element.style.height = `${30 + 10 * i}px`;
+    element.style.backgroundColor = getRandomHexColor();
+    totalElements.push(element);
+  }
+  return totalElements;
+};
+
+createBtn.addEventListener("click", () => {
+  const newBoxes = createBoxes(inputValue.value);
+  boxes.append(...newBoxes);
+});
+
+const destroyBoxes = () => {
+  boxes.innerHTML = "";
+};
+
+destroyBtn.addEventListener("click", () => {
+  destroyBoxes.call();
+});
